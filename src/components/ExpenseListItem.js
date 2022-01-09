3e11036f1643cfcc4073062165b9df916a776f2e
leemacpherson <leemacpherson@msn.com>
2022-01-09 08:38:47 -0800

    deleted the numeral package

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
-import numeral from "numeral";
+//import numeral from "numeral";
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
@@ -9,9 +9,7 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
       <h3>{description}</h3>
     </Link>
     <p>
-      test change
-      {numeral(amount / 100).format("$0,0.00")}-
-      {moment(createdAt).format("MMMM Do, YYYY")}
+      {amount / 100}-{moment(createdAt).format("MMMM Do, YYYY")}
     </p>
   </div>
 );
