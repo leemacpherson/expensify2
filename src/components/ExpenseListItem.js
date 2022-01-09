a034e98a62b04ca6a4ec007c18f540b9130d06e8
leemacpherson <leemacpherson@msn.com>
2022-01-09 08:14:24 -0800

    trying to get numeral into yarn/cashe

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -9,6 +9,7 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
       <h3>{description}</h3>
     </Link>
     <p>
+      test change
       {numeral(amount / 100).format("$0,0.00")}-
       {moment(createdAt).format("MMMM Do, YYYY")}
     </p>
